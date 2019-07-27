667fcb5dd3b5d6e172c819eaa01a08ed04c5ad72
yesrimao <18566191953@163.com>
2019-07-27 11:37:02 +0800

    提交index.html视口设置/reset.css文件/border.css文件

--- travel_vue/src/main.js
+++ travel_vue/src/main.js
@@ -3,9 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移入移动端解决300ms触屏延时问题的依赖包文件
+import fastClick from 'fastclick'
+// 引入移动端重置css样式
+import './assets/styles/reset.css'
+// 移入移动端解决1px问题css样式文件
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+// 在document.body中绑定fastClick
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
