2752172396b8877fbc4e04b15976f2b292aa5788
Yesirmao <18566191953@163.com>
2019-07-29 23:25:26 +0800

    新增城市页面搜索框

---
+++ travel_vue/src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+		<input class="input-search" type="text" placeholder="输入城市名或拼音">
+	</div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+	@import '~^@/varibles.styl'
+	.search
+		height: .72rem
+		padding: 0 .1rem
+		background: $bgColor
+		.input-search
+			box-sizing: border-box
+			padding: 0 .1rem
+			width: 100%
+			height: .62rem
+			line-height: .62rem
+			border-radius: .06rem
+			text-align: center
+			color: #666
+</style>
