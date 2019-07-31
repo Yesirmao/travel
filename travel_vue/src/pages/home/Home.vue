9f65144f70f5731b4983970d836a14360c477945
Yesirmao <18566191953@163.com>
2019-07-31 23:00:14 +0800

    使用keep-alive对页面请求进行优化/使用activated周期函数来实现切花不同城市发送不同的ajax请求

--- travel_vue/src/pages/home/Home.vue
+++ travel_vue/src/pages/home/Home.vue
@@ -13,6 +13,7 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
@@ -22,6 +23,7 @@ export default {
   name: 'Home',
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       itemList: [],
       recommendList: [],
@@ -35,11 +37,15 @@ export default {
     HomeRecommond,
     HomeWeekend
   },
+  computed: {
+    // 当切换城市时，在vuex中获取对应的城市的值
+    ...mapState(['city'])
+  },
   methods: {
     // 发送获取首页数据的ajax请求
     getHomeInfo () {
       // 此时的api经过配置转发后会被替换为/static/mock
-      this.axios.get('/api/index.json').then(this.getHomeInfoSuccess)
+      this.axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSuccess)
     },
     // 获取首页请求数据
     getHomeInfoSuccess (res) {
@@ -55,7 +61,18 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  // 当是使用keep-alive时，会新增一个activated生命周期函数，当页面重新被显示的时候，activated会重新执行，即：当我们首次进入首页时该方法会被
+  // 执行，当我们从城市选择页面切换会首页时，首页重新加载时，也会被重新执行。由于返回时的数据时从内存中获取的，因此mounted不会被执行。所以此时
+  // 的mouted的钩子函数会造成一个问题：切换到不同的城市时，首页不会加载不同的ajax请求
+  // 此时，我们需要实现的功能为：当当前城市和缓存在lastCity中的城市不相等时，说明不是相同的请求，此时需要重新发送ajax请求，加载页面数据。
+  activated () {
+    if (this.city !== this.lastCity) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
