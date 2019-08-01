6140639c4e9d3bcb0f7274976216da06bd4524ff
yesrimao <18566191953@163.com>
2019-08-01 16:22:52 +0800

    添加热点推荐的详情页

---
+++ travel_vue/src/pages/detail/components/Banner.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="banner">
+		<img src="http://img1.qunarzz.com/sight/p0/1907/8d/8da769b8a045ec98a3.img.jpg_600x330_43ca5751.jpg"
+		class="banner-img" alt="深圳欢乐谷">
+		<div class="banner-info">
+			<div class="banner-title">深圳欢乐谷(AAAAA景区)</div>
+			<div class="banner-number">
+				<span class="iconfont banner-icon">&#xe632;</span>
+				<span>39</span>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+<style lang="stylus" scoped>
+	.banner
+		position: relative
+		overflow: hidden
+		height: 0
+		padding-bottom: 55%
+		.banner-img
+			width: 100%
+		.banner-info
+			display: flex
+			position: absolute
+			left: 0
+			right: 0
+			bottom: 0
+			line-height: .6rem
+			color: #fff
+			.banner-title
+				flex: 1
+				font-size: .32rem
+				padding: 0 .2rem
+			.banner-number
+				height: .32rem
+				line-height: .32rem
+				padding: 0 .4rem
+				margin-top: .14rem
+				border-radius: .2rem
+				background: rgb(0, 0, 0, 0, .8)
+				font-size: .24rem
+				.banner-icon
+					font-size: .24rem
+</style>
--- travel_vue/src/pages/home/components/Recommond.vue
+++ travel_vue/src/pages/home/components/Recommond.vue
@@ -2,14 +2,19 @@
   <div>
 		<div class="title">{{hotRecommend}}</div>
 		<ul>
-			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+			<router-link
+			class="item border-bottom"
+			v-for="item of recommendList"
+			:key="item.id"
+			tag="li"
+			:to="'/detail/?id=' + item.id">
 				<img :src="item.imgUrl" class="item-img">
 				<div class="item-info">
 					<p class="item-title">{{item.title}}</p>
 					<p class="item-desc">{{item.desc}}</p>
 					<button class="item-button">{{seeDetail}}</button>
 				</div>
-			</li>
+			</router-link>
 		</ul>
   </div>
 </template>
